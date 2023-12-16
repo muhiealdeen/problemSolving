f44e85d09b6ebb49e69ce9198454ad2f928a9d4f
Muhie Aldeen <zezo1998@hotmail.com>
2023-12-16 18:44:46 +0100

    solve day2- 1

--- advent-of-code/day-2/day2-1.js
+++ advent-of-code/day-2/day2-1.js
@@ -1,37 +1,3 @@
-// // // const { count } = require('console');
-// // const fs = require('fs');
-
-// // const maxValues = {
-// //   red: 12,
-// //   green: 13,
-// //   blue: 14,
-// // };
-// // function readFile(file) {
-// //   const lines = fs.readFileSync(file, 'utf-8').trim().split('\n');
-// //   // Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-// //   lines.map((line) => {
-// //     const sets = line
-// //       .split(': ')[1]
-// //       .split('; ')
-// //       .map((set) => {
-// //         const pulls = set.split(', ');
-// //         console.log('pulls', pulls);
-// //         return pulls.every((pull) => {
-// //           const [value, color] = pull.split(' ');
-// //           // console.log('=============', value, color);
-// //           // // console.log('NUMBER!!!!', Number(value));
-// //           // console.log('MAX??????', maxValues[color]);
-// //           // // return maxValues[color] >= Number(value);
-// //         });
-// //       });
-// //     console.log('sets', sets);
-// //   });
-
-// //   // return lines;
-// // }
-
-// // console.log('LLLLLLL', readFile('./input.txt'));
-
 // const fs = require('fs');
 
 // const maxValues = {
