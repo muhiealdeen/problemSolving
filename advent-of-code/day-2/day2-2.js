d02ff8c56ab3d2401ef71840da60bbe3cf3b8e06
Muhie Aldeen <zezo1998@hotmail.com>
2023-12-21 17:21:27 +0100

    I made some changes in the code to make it works

--- advent-of-code/day-2/day2-2.js
+++ advent-of-code/day-2/day2-2.js
@@ -1,88 +1,3 @@
-// const path = require('path');
-// const fs = require('fs');
-
-// const input = fs
-//   .readFileSync(path.join(__dirname, 'example.txt'), 'utf8')
-//   .toString()
-//   .trim()
-//   .split('\n');
-
-// function parse(input) {
-//   return input.map((line, index) => {
-//     const match = /Game (\d+): (.+)$/.exec(line);
-
-//     if (!match) {
-//       console.error(`Error parsing line: ${line}`);
-//       return null;
-//     }
-
-//     const [, id, rest] = match;
-
-//     const picks = rest.split(/[;,] /);
-//     const cubesPicked = picks.map((pick) => {
-//       const [num, color] = pick.split(' ');
-//       return [parseInt(num, 10) || 0, color || ''];
-//     });
-
-//     return {
-//       id: parseInt(id, 10),
-//       minCubes: cubesPicked.reduce(
-//         (acc, [num, color]) => {
-//           acc[color] = (acc[color] || 0) + num;
-//           return acc;
-//         },
-//         { blue: 0, red: 0, green: 0 },
-//       ),
-//     };
-//   });
-// }
-
-// const games = parse(input.filter(Boolean));
-
-// let sum = 0;
-// for (let game of games.filter(Boolean)) {
-//   const { minCubes } = game;
-//   const power = minCubes.red * minCubes.blue * minCubes.green;
-//   sum += power;
-// }
-
-// console.log(sum);
-// ------------------------------------------------------------
-
-// const fs = require('fs');
-
-// const maxValues = {
-//   red: 12,
-//   green: 13,
-//   blue: 14,
-// };
-// function readFile(file) {
-//   const lines = fs.readFileSync(file, 'utf-8').trim().split('\n');
-//   // Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-//   lines.map((line) => {
-//     const sets = line
-//       .split(': ')[1]
-//       .split('; ')
-//       .map((set) => {
-//         const pulls = set.split(', ');
-//         console.log('pulls', pulls);
-//         return pulls.every((pull) => {
-//           const [value, color] = pull.split(' ');
-//           // console.log('=============', value, color);
-//           // // console.log('NUMBER!!!!', Number(value));
-//           // console.log('MAX??????', maxValues[color]);
-//           // // return maxValues[color] >= Number(value);
-//         });
-//       });
-//     console.log('sets', sets);
-//   });
-
-//   // return lines;
-// }
-
-// console.log('LLLLLLL', readFile('./input.txt'));
-// =============================================================
-
 const fs = require('fs');
 
 function readFile2(file) {
@@ -94,27 +9,26 @@ function readFile2(file) {
       green: 0,
       blue: 0,
     };
+    console.log(line, 'line');
     line
       .split(': ')[1]
       .split('; ')
       .forEach((set) => {
         const pulls = set.split(', ');
-        // console.log(pulls, 'pulls');
+        console.log(pulls, 'pulls');
         return pulls.map((pull) => {
-          const [value, color] = pull.split(' ');
+          const [value, color] = pull.trim().split(' ');
           // console.log('=============', value, color);
           // console.log('NUMBER!!!!', Number(value));
           // console.log('MAX??????', maxValues[color]);
-          if (maxValues[color] < Number(value)) {
-            maxValues[color] = Number(value);
-          }
+          maxValues[color] = Math.max(maxValues[color], Number(value));
         });
       });
     // console.log(maxValues, 'maxValues');
     return maxValues.red * maxValues.green * maxValues.blue;
   });
 }
-const powerArray = readFile2('./input.txt');
+const powerArray = readFile2('./example.txt');
 const sumOfPowers = powerArray.reduce((total, number) => total + number, 0);
 
 console.log('The sum of the power of these sets', powerArray);
