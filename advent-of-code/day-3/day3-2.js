5e043a7b70744aceb16706a66c80fc5ed95d0bbc
Muhie Aldeen <zezo1998@hotmail.com>
2023-12-26 18:47:17 +0100

    I copied the loops from part one

--- advent-of-code/day-3/day3-2.js
+++ advent-of-code/day-3/day3-2.js
@@ -10,4 +10,19 @@ fs.readFile('./examlpe.txt', 'utf-8', (err, data) => {
   const totalRows = schematicRows.length;
   const totalCols = schematicRows[0].length;
   console.log(schematicRows, totalRows, totalCols);
+  for (let i = 0; i < totalRows; i++) {
+    for (let j = 0; j < totalCols; j++) {
+      const currentChar = '' + schematicRows[i][j];
+      if (isNaN(currentChar)) continue;
+
+      let partNumber = currentChar;
+      for (let col = j + 1; col < totalCols; col++) {
+        const nextChar = schematicRows[i][col];
+        if (Number.isInteger(parseInt(nextChar))) {
+          partNumber += nextChar;
+        } else {
+          j = col;
+          break;
+        }
+      }
 });
